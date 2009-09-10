bcd43f17192f73dae35a4ca6ff30d29c9ba18464
Evan David Light <evan@tiggerpalace.com>
2009-09-10 13:52:41 -0400

    Reorganizing thoughts in scenario pending specs

--- test/scenario_test.rb
+++ test/scenario_test.rb
@@ -12,49 +12,52 @@ class ScenarioTest < Test::Unit::TestCase
       end
     end
 
-    context "when instantiated" do
-      should "not raise an error when given a String" 
-      should "raise an error if not given a String"
-    end
-
-    context "without a block" do
-      should "create a test that outputs that the scenario is pending"
-    end
-
-    context "with a block" do
-      should "create a test that calls the block"
-
-      context "when calling the block" do
-        %[given when then].each do |stmt|
-          context "and no #{stmt}s are present" do
-            should "raise a DSL syntax error that a #{stmt} is not present"
-          end
+    context "when instantiated" do 
+      context "with only a String" do
+        setup do 
+          @scenario = Scenario.new("foobar")
         end
 
-        context "and all givens occur before all whens" do
-          context "and all whens occur before all thens" do
-            should "not raise an error"
-          end
-
-          context "and all whens do not occur before all thens" do
-            should "raise a DSL syntax error"
-          end
+        should "be pending" do
+          assert(@scenario.pending?)
         end
+      end
 
-        context "where not all givens occur before all whens" do
-          context "and all whens occur before all thens" do
-            should "raise a DSL syntax error"
+      context "with a block" do
+        should "validate the semantics of the code in the block" 
+
+        context "when validating the semantics of the code in the block" do
+          %[given when then].each do |stmt|
+            context "and no #{stmt}s are present" do
+              should "raise a DSL syntax error that a #{stmt} is not present"
+            end
           end
 
-          context "and all whens do not occur before all thens" do
-            should "raise a DSL syntax error"
-          end
-        end
+          context "and all givens occur before all whens" do
+            context "and all whens occur before all thens" do
+              should "not raise an error"
+            end
 
-        context "where givens, whens, are present and in the correct order" do
-          %w[given when then].each do |stmt|
-            context "but a #{stmt} does not have a block" do
-              should "declare the scenario pending"
+            context "and all whens do not occur before all thens" do
+              should "raise a DSL syntax error"
+            end
+          end
+
+          context "where not all givens occur before all whens" do
+            context "and all whens occur before all thens" do
+              should "raise a DSL syntax error"
+            end
+
+            context "and all whens do not occur before all thens" do
+              should "raise a DSL syntax error"
+            end
+          end
+
+          context "where givens, whens, are present and in the correct order" do
+            %w[given when then].each do |stmt|
+              context "but a #{stmt} does not have a block" do
+                should "declare the scenario pending"
+              end
             end
           end
         end
