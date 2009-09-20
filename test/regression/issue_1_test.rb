ac12446f89766def1485a74a6de15a360216115b
Evan David Light <evan@tiggerpalace.com>
2009-09-20 16:10:27 -0400

    Improved clarity of regression test

--- test/regression/issue_1_test.rb
+++ test/regression/issue_1_test.rb
@@ -1,16 +1,16 @@
 require File.join(File.dirname(__FILE__), "..", "test_helper")
 
+module GivenSomething
+  def given_something
+    Given "something" do; end
+  end
+end
+
 # http://github.com/elight/coulda/issues#issue/1
 class Issue1Test < Test::Unit::TestCase
   context "A Feature that has a Scenario that invokes a 'Given/When/Then' called from a method" do
     setup do
       pendings_are_errors
-
-      module GivenSomething
-        def given_something
-          Given "something" do; end
-        end
-      end
 
       @feature_without_errors = Feature "Issue 1 feature" do
         include GivenSomething
